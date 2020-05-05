c026fe596f3a2f0e305d2776bd4280f6464ce894
Kyle Price <kyleprice@yahoo.com>
2020-05-05 13:34:56 -0400

    re-org dist and deps

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = (env) => {
     return {
         entry: './src/app.js',
         output: {
-            'path': path.join(__dirname, 'public'),
+            'path': path.join(__dirname, 'public', 'dist'),
             'filename': 'bundle.js'
         },
         mode: 'development',
@@ -33,6 +33,7 @@ module.exports = (env) => {
         devtool: isProd ? 'source-map' : 'inline-source-map',
         devServer: {
             contentBase: path.join(__dirname, 'public'),
+            publicPath: '/dist/',
             historyApiFallback: true
         }
     };
