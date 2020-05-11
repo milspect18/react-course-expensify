2190b9a139bed96f8b4b3391ebafd14e8bc024e3
Kyle Price <kyleprice@yahoo.com>
2020-05-11 18:34:39 -0400

    fixe edit expense page test for new async firebase remover

--- src/tests/components/EditExpensePage.test.js
+++ src/tests/components/EditExpensePage.test.js
@@ -3,17 +3,17 @@ import { shallow } from 'enzyme';
 import { EditExpensePage } from '../../components/EditExpensePage';
 import expenses from '../fixtures/expenses';
 
-let editExpenseSpy, removeExpenseSpy, historySpy, wrapper;
+let editExpenseSpy, startRemoveExpense, historySpy, wrapper;
 
 beforeEach(() => {
     editExpenseSpy = jest.fn();
-    removeExpenseSpy = jest.fn();
+    startRemoveExpense = jest.fn();
     historySpy = { push: jest.fn() };
     wrapper = shallow(
         <EditExpensePage
             expenseToEdit={expenses[1]}
             editExpense={editExpenseSpy}
-            removeExpense={removeExpenseSpy}
+            startRemoveExpense={startRemoveExpense}
             history={historySpy}
         />
     );
@@ -26,7 +26,7 @@ test('Should render edit expense page', () => {
 test('Should ensure on click is firing', () => {
     wrapper.find('button').simulate('click');
 
-    expect(removeExpenseSpy).lastCalledWith(expenses[1].id);
+    expect(startRemoveExpense).lastCalledWith(expenses[1].id);
     expect(historySpy.push).lastCalledWith('/');
 });
 
